fcd22174e45bd655d3e5798dd9e955f1c51f8fff
xdexst <rnoy10303@gmail.com>
2024-02-20 16:15:31 +0400

    Работа с объктом Road

--- main.cpp
+++ main.cpp
@@ -14,3 +14,23 @@ Road::Road()
 	length = 111.1;
 	width = 11111;
 }
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	cout << "TeTest v1.2: Ok.\n"; 
+
+	Road road;
+	cout << " Length: " << road.length << endl;
+	cout << "Width: " << road.width << endl;
+
+	cout << "Введите длину дороги: " << endl;
+	cin >> road.length;
+	cout << "Введите ширину дороги: " << endl;
+	cin >> road.width;
+	
+	return 0;
+
+
+
+}
